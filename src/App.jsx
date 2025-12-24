da716999f8f5f421fe69c9c942e2b60a02c17358
samarthkumar096-commits <samarthkumar096@gmail.com>
2025-12-24 10:50:21 +0530

    Add download page route

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Resume from './pages/Resume'
 import ATSScore from './pages/ATSScore'
-import Profile from './pages/Profile'
 import Pricing from './pages/Pricing'
-import Success from './pages/Success'
-import Cancel from './pages/Cancel'
+import Download from './pages/Download'
 
 function App() {
   return (
@@ -14,10 +12,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/ats-score" element={<ATSScore />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="/pricing" element={<Pricing />} />
-        <Route path="/success" element={<Success />} />
-        <Route path="/cancel" element={<Cancel />} />
+        <Route path="/download" element={<Download />} />
       </Routes>
     </Router>
   )
