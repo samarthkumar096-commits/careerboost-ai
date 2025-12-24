12e0dd157183b99e9679788fdd63ddb679abcd8e
samarthkumar096-commits <samarthkumar096@gmail.com>
2025-12-24 11:50:19 +0530

    Update App with authentication routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
 import Home from './pages/Home'
 import Resume from './pages/Resume'
 import ATSScore from './pages/ATSScore'
 import Pricing from './pages/Pricing'
 import Download from './pages/Download'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/ats-score" element={<ATSScore />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/download" element={<Download />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/ats-score" element={<ATSScore />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/download" element={<Download />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   )
 }
 
